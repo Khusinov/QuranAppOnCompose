329c7beb8e6ed058d0a43884bdfe8829c8f18a96
usermac <shakhriyordev@gmail.com>
2023-10-05 14:34:12 +0500

    some changes

--- app/src/main/java/uz/khusinov/quranapponcompose/MainActivity.kt
+++ app/src/main/java/uz/khusinov/quranapponcompose/MainActivity.kt
@@ -3,15 +3,25 @@ package uz.khusinov.quranapponcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import uz.khusinov.quranapponcompose.ui.theme.QuranAppOnComposeTheme
@@ -22,21 +32,43 @@ class MainActivity : ComponentActivity() {
         setContent {
             MessageCard(Message("Shaxriyor", "Assalomu alaykum bo'lajak dasturchilar "))
         }
-
-
     }
 
     data class Message(val author: String, val body: String)
 
     @Composable
     fun MessageCard(msg: Message) {
-        Text(text = msg.author)
-        Text(text = msg.body)
+        Row(modifier = Modifier.padding(all = 8.dp)) {
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+                    .border(1.5.dp, MaterialTheme.colorScheme.secondary)
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Column() {
+                Text(
+                    text = msg.author,
+                    color = MaterialTheme.colorScheme.secondary
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(text = msg.body)
+            }
+
+
+
+
+        }
+
     }
 
     @Preview
     @Composable
     fun previewMessageCard() {
-        MessageCard(msg = Message("Shaxriy" , "textxttxtxtxtxx"))
+        MessageCard(msg = Message("Shaxriy", "textxttxtxtxtxx"))
     }
 }
